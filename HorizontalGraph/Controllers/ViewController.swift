116e0fb3c64a15f0492997ec9c9495db857137c0
Marcelo Sampaio <marceloazevedo@r3k0mbp.local>
2019-03-01 12:57:41 -0300

    working on left graph commit 💿

--- HorizontalGraph/Controllers/ViewController.swift
+++ HorizontalGraph/Controllers/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var horizontalGraph: HorizontalGraph!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set graph view background color
+        self.horizontalGraph.leftGraphColor(UIColor.green)
+        
     }
 
 
---
+++ HorizontalGraph/Horizontal Graph/HorizontalGraph.swift
@@ -0,0 +1,41 @@
+//
+//  HorizontalGraph.swift
+//  HorizontalGraph
+//
+//  Created by Marcelo Sampaio on 01/03/19.
+//  Copyright © 2019 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalGraph: UIView {
+    
+    // MARK: - Outlets
+    @IBOutlet var view: UIView!
+    
+    // left graph
+    @IBOutlet weak var leftGraphView: UIView!
+    
+    // left graph constraints
+    @IBOutlet weak var leftGraphLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftGraphTrailingConstraint: NSLayoutConstraint!
+    
+
+    // MARK: - Constructor
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        _ = Bundle.main.loadNibNamed("HorizontalGraph", owner: self, options: nil)?[0] as! UIView
+        
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    // MARK: - Public Methods
+    public func leftGraphColor(_ color: UIColor) {
+        self.leftGraphView.backgroundColor = color
+    }
+    
+
+
+}
