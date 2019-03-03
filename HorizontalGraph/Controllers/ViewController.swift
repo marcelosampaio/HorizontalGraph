b849c9db44c3518868feb942c7175f43baaf0cc2
Marcelo Sampaio <marceloazevedo@r3k0mbp.local>
2019-03-02 21:13:05 -0300

    source code clean up ✴️

--- HorizontalGraph/Controllers/ViewController.swift
+++ HorizontalGraph/Controllers/ViewController.swift
@@ -16,55 +16,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        // set graph view background color
-//        self.horizontalGraph.layer.borderColor = UIColor.black.cgColor
-//        self.horizontalGraph.layer.borderWidth = 1.5
-//
-//        // ------------------------
-//        // left value
-//        let valueLeft : CGFloat = 50.00      // 50%
-//        let sizeLeft : CGFloat = (valueLeft * self.horizontalGraph.bounds.width) / 100.00
-//        let leadingLeft : CGFloat = 0.00
-//        let trailingLeft : CGFloat = 0.00
-//
-//        // center value
-//        let valueCenter : CGFloat = 50.00      // 20%
-//        let sizeCenter : CGFloat = (valueCenter * self.horizontalGraph.bounds.width) / 100.00
-//        let leadingCenter : CGFloat = sizeLeft
-//        let trailingCenter : CGFloat = 0
-//
-//
-//        // right value
-////        let valueRight : CGFloat = 30.00      // 20%
-////        let sizeRight : CGFloat = (valueRight * self.horizontalGraph.bounds.width) / 100.00
-//        let leadingRight : CGFloat = sizeLeft + sizeCenter
-//        let trailingRight : CGFloat = 0
-//
-//
-//
-//        // -------------------------
-//
-//
-//
-//
-//
-//        // left graph
-//        self.horizontalGraph.leftGraphColor(UIColor.green)
-//        self.horizontalGraph.leftGraphConstraints(leading: leadingLeft, trailing: trailingLeft)
-//
-//        // center graph
-//        self.horizontalGraph.centerGraphColor(UIColor.red)
-//        self.horizontalGraph.centerGraphConstraints(leading: leadingCenter, trailing: trailingCenter)
-//
-//        // right graph
-//        self.horizontalGraph.rightGraphColor(UIColor.brown)
-//        self.horizontalGraph.rightGraphConstraints(leading: leadingRight, trailing: trailingRight)
-//
-//
-//
-//        self.horizontalGraph.layoutIfNeeded()
-        
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -74,7 +25,6 @@ class ViewController: UIViewController {
         self.horizontalGraph.layer.borderColor = UIColor.black.cgColor
         self.horizontalGraph.layer.borderWidth = 1.5
         
-        // ------------------------
         // left value
         let valueLeft : CGFloat = 33.33      // 50%
         let sizeLeft : CGFloat = (valueLeft * self.horizontalGraph.bounds.width) / 100.00
@@ -89,18 +39,8 @@ class ViewController: UIViewController {
         
         
         // right value
-        //        let valueRight : CGFloat = 30.00      // 20%
-        //        let sizeRight : CGFloat = (valueRight * self.horizontalGraph.bounds.width) / 100.00
         let leadingRight : CGFloat = sizeLeft + sizeCenter
         let trailingRight : CGFloat = 0
-        
-        
-        
-        // -------------------------
-        
-        
-        
-        
         
         // left graph
         self.horizontalGraph.leftGraphColor(UIColor.green)
